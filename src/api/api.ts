ca1e1dc29f65b6113e597f6925dd01879aeec41c
ThembaMtshelwane <thembamm3@gmail.com>
2024-10-03 11:49:13 +0200

    api update

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,14 +1,11 @@
 import axios from "axios";
 import { Task } from "../definitions";
 
-const PORT = 9000;
-const LOCAL_DEV = `http://localhost:${PORT}/api/tasks`;
-const REMOTE = "https://server-indol-kappa.vercel.app/api/tasks";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const getAllTasks = async () => {
   try {
-    // const res = await axios.get(LOCAL_DEV);
-    const res = await axios.get(REMOTE);
+    const res = await axios.get(BASE_URL);
     return res.data;
   } catch (error) {
     console.error("Error fetching all tasks", error);
@@ -19,8 +16,7 @@ export const addTask = async (task: { description: string }) => {
   console.log("new task to add", task);
 
   try {
-    const res = await axios.post(REMOTE, task);
-    // const res = await axios.post(LOCAL_DEV, task);
+    const res = await axios.post(BASE_URL, task);
     console.log(res.data);
   } catch (error) {
     console.error("Error adding task", error);
@@ -29,8 +25,7 @@ export const addTask = async (task: { description: string }) => {
 
 export const editTask = async (task: Task) => {
   try {
-    const res = await axios.put(REMOTE, task);
-    // const res = await axios.put(LOCAL_DEV, task);
+    const res = await axios.put(BASE_URL, task);
 
     console.log(res.data);
     return res.data;
@@ -41,7 +36,7 @@ export const editTask = async (task: Task) => {
 
 export const deleteTask = async (id: string) => {
   try {
-    const res = await axios.delete(`${REMOTE}/${id}`);
+    const res = await axios.delete(`${BASE_URL}/${id}`);
     // const res = await axios.delete(`${LOCAL_DEV}/${id}`);
     console.log(res.data);
     return res.data;
